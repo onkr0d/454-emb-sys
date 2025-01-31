08dbce1a7ae859cc25a6172af0d74ba217618944
TigerLiu <tigerliu@bu.edu>
2025-01-31 17:00:13 -0500

    feat: finish lab1
    yippee

--- Lab1.X/joystick.h
+++ Lab1.X/joystick.h
@@ -20,23 +20,20 @@
 /* BTN1 (BUTTON 1) is the "fire" button on the koystick */
 /* BTN2 (BUTTON 2) is the "thumb" button on the koystick */
 
-#error BTN1_PRESSED is not implemented. Implement it below and delete this line
-#define BTN1_PRESSED() \
-    (/* insert expression to test if BTN1 is pressed here */)
+//#error BTN1_PRESSED is not implemented. Implement it below and delete this line
+#define BTN1_PRESSED() (PORTEbits.RE8 == 0)
 
-#error BTN1_RELEASED is not implemented. Implement it below and delete this line
-#define BTN1_RELEASED() \
-    (/* insert expression to test if BTN1 is released here */)
+//#error BTN1_RELEASED is not implemented. Implement it below and delete this line
+#define BTN1_RELEASED() (PORTEbits.RE8 == 1)
 
-#error BTN2_PRESSED is not implemented. Implement it below and delete this line
-#define BTN2_PRESSED() \
-    (/* insert expression to test if BTN2 is pressed here */)
+//#error BTN2_PRESSED is not implemented. Implement it below and delete this line
+#define BTN2_PRESSED() (PORTDbits.RD10 == 0)
 
-#error BTN2_RELEASED is not implemented. Implement it below and delete this line
-#define BTN2_RELEASED() \
-    (/* insert expression to test if BTN2 is released here */)
+//#error BTN2_RELEASED is not implemented. Implement it below and delete this line
+#define BTN2_RELEASED() (PORTDbits.RD10 == 1)
 
 /* === Function prototypes implemented in joystic.h go here === */
+
 
 
 #endif /* __JOYSTICK_H */
--- Lab1.X/main.c
+++ Lab1.X/main.c
@@ -18,6 +18,9 @@
 
 #include "lcd.h"
 #include "types.h"
+#include "led.h"
+#include <xc.h>   // Contains Nop() declaration
+#include "joystick.h"
 
 /* Initial configuration by EE */
 // Primary (XT, HS, EC) Oscillator with PLL
@@ -39,7 +42,7 @@ int main(){
 	lcd_initialize();
 	lcd_clear();
     
-    // Task 1
+    // Task 1: Display Group Name
     // lcd_locate (column, row)
 	lcd_locate(0,0);
 	lcd_printf("Tiger");
@@ -49,14 +52,80 @@ int main(){
     
     lcd_locate(0,2);
     lcd_printf("Tom");
-
-    // Task 2
-  
-
-	
+    /*
+     Task 2:
+     * 1. Blink LED4 3 times
+     * 2. 1 second between blinks
+     * 3. LED4 off after 3 blinks
+     */
+//    int led4_count = 0;
+//    CLEARBIT(LED4_TRIS); // Set Pin to Output
+//    while(led4_count < 3) {
+//        SETLED(LED4_PORT);
+//        Nop();
+//        __delay_ms(100);
+//        
+//        CLEARLED(LED4_PORT);
+//        Nop();
+//        __delay_ms(1000);
+//        
+//        led4_count++;
+//    };
+//    
+    
+    
+    CLEARBIT(LED1_TRIS); // Set Pin to Output
+    CLEARBIT(LED2_TRIS); // Set Pin to Output
+    CLEARBIT(LED3_TRIS);
+    
+    // configuration of joystick registers according to Lab Manual 4.6.2, "Button #1 is located on PORTE PIN8..."
+    SETBIT(TRISEbits.TRISE8);
+    SETBIT(INTCON2bits.INT1EP);
+    SETBIT(AD1PCFGHbits.PCFG20);
+    
+    unsigned int buttonCount = 0;
+    unsigned int holdBuffer = 0;
+    unsigned int alreadyPressed = 0;
+    
+    while (1) {
+        if (BTN1_PRESSED()) {
+            // no point in increasing the buffer past 10, set to 20 here for fun
+            if (holdBuffer <= 20) {
+                holdBuffer++;
+            }
+            if (holdBuffer > 10 && !alreadyPressed) {
+                SETLED(LED1_PORT);
+                Nop();
+                buttonCount++;
+                alreadyPressed = 1;
+            }
+        } else {
+            holdBuffer = 0;
+            alreadyPressed = 0;
+            CLEARLED(LED1_PORT);
+            Nop();
+        }
+        if (BTN2_PRESSED()) {
+            SETLED(LED2_PORT); 
+            Nop();
+        } else {
+            CLEARLED(LED2_PORT);
+            Nop();
+        }
+        if (BTN1_PRESSED() ^ BTN2_PRESSED()) {
+            SETLED(LED3_PORT);
+        } else {
+            CLEARLED(LED3_PORT);
+        }
+        lcd_locate(0,3);
+        lcd_printf("%d %x", buttonCount, buttonCount);
+        lcd_locate(0,3);
+    }
+    
+  	
 	/* Do nothing */
 	while(1){
-		
+
 	}
     
     return 0;
