3659195c6818b96e2b6a171db52f1e1681a71a5e
TigerLiu <tigerliu@bu.edu>
2025-02-08 17:32:42 -0500

    lab 2 task 3 in prog

--- Lab2.X/main.c
+++ Lab2.X/main.c
@@ -42,41 +42,11 @@ int main(){
 	lcd_initialize();
 	lcd_clear();
     
-    // Task 1: Display Group Name
-    // lcd_locate (column, row)
-	lcd_locate(0,0);
-	lcd_printf("Tiger");
-    
-    lcd_locate(0,1);
-    lcd_printf("Ivan");
-    
-    lcd_locate(0,2);
-    lcd_printf("Tom");
-    /*
-     Task 2:
-     * 1. Blink LED4 3 times
-     * 2. 1 second between blinks
-     * 3. LED4 off after 3 blinks
-     */
-//    int led4_count = 0;
-//    CLEARBIT(LED4_TRIS); // Set Pin to Output
-//    while(led4_count < 3) {
-//        SETLED(LED4_PORT);
-//        Nop();
-//        __delay_ms(100);
-//        
-//        CLEARLED(LED4_PORT);
-//        Nop();
-//        __delay_ms(1000);
-//        
-//        led4_count++;
-//    };
-//    
-    
-    
+
     CLEARBIT(LED1_TRIS); // Set Pin to Output
     CLEARBIT(LED2_TRIS); // Set Pin to Output
-    CLEARBIT(LED3_TRIS);
+    CLEARBIT(LED3_TRIS); // Set Pin to Output
+    CLEARBIT(LED4_TRIS); // Set Pin to Output
     
     // configuration of joystick registers according to Lab Manual 4.6.2, "Button #1 is located on PORTE PIN8..."
     SETBIT(TRISEbits.TRISE8);
@@ -87,46 +57,87 @@ int main(){
     unsigned int holdBuffer = 0;
     unsigned int alreadyPressed = 0;
     
+//  Timer 2 code
+    CLEARBIT( T2CONbits.TON ) ;
+    CLEARBIT(T2CONbits.TCS);
+    CLEARBIT( T2CONbits.TGATE);
+    TMR2 = 0;
+    T2CONbits.TCKPS = 0b11;
+    CLEARBIT(IFS0bits.T2IF) ;
+    CLEARBIT( IEC0bits.T2IE);
+    PR2 = 16000 ; 
+    SETBIT(T2CONbits.TON);
+    SETBIT( IEC0bits.T2IE);
+   
+    // Timer 1 code
+    //enable LPOSCEN
+    __builtin_write_OSCCONL(OSCCONL | 2);
+    T1CONbits.TON = 0; //Disable Timer
+    T1CONbits.TCS = 1; //Select external clock
+    T1CONbits.TSYNC = 0; //Disable Synchronization
+    T1CONbits.TCKPS = 0b00; //Select 1:1 Prescaler
+    TMR1 = 0x00; //Clear timer register
+    PR1 = 32767; //Load the period value
+    IPC0bits.T1IP = 0x01; // Set Timer1 Interrupt Priority Level
+    IFS0bits.T1IF = 0; // Clear Timer1 Interrupt Flag
+    IEC0bits.T1IE = 1;// Enable Timer1 interrupt
+    T1CONbits.TON = 1;// Start Timer
+    
+      
+      
+    int flip0 = 0;
     while (1) {
-        if (BTN1_PRESSED()) {
-            // no point in increasing the buffer past 10, set to 20 here for fun
-            if (holdBuffer <= 20) {
-                holdBuffer++;
+  
+        flip0 = 1 - flip0;
+        if( flip0 ) {
+            SETLED( LED4_PORT );
+            Nop();
+            __delay_ms(300); 
             }
-            if (holdBuffer > 10 && !alreadyPressed) {
-                SETLED(LED1_PORT);
-                Nop();
-                buttonCount++;
-                alreadyPressed = 1;
-            }
-        } else {
-            holdBuffer = 0;
-            alreadyPressed = 0;
-            CLEARLED(LED1_PORT);
+        else {
+            CLEARLED(LED4_PORT);
             Nop();
+            __delay_ms(300); 
         }
-        if (BTN2_PRESSED()) {
-            SETLED(LED2_PORT); 
-            Nop();
-        } else {
-            CLEARLED(LED2_PORT);
-            Nop();
-        }
-        if (BTN1_PRESSED() ^ BTN2_PRESSED()) {
-            SETLED(LED3_PORT);
-        } else {
-            CLEARLED(LED3_PORT);
-        }
-        lcd_locate(0,3);
-        lcd_printf("%d %x", buttonCount, buttonCount);
-        lcd_locate(0,3);
     }
     
-  	
-	/* Do nothing */
-	while(1){
-
-	}
     
     return 0;
+}
+
+int flip2 = 0;
+
+void __attribute__(( __interrupt__ )) _T2Interrupt(void)
+{
+    flip2 = 1 - flip2;
+    if( flip2 ) {
+        SETLED( LED1_PORT );
+        Nop();
+        }
+    else {
+
+        CLEARLED(LED1_PORT);
+        Nop();
+    }    
+ CLEARBIT(IFS0bits.T2IF) ;
+    
+    
+}
+
+int flip1 = 0;
+void __attribute__(( __interrupt__ )) _T1Interrupt(void)
+{
+    flip1 = 1 - flip1;
+    if( flip1) {
+        SETLED( LED2_PORT );
+        Nop();
+        }
+    else {
+
+        CLEARLED(LED2_PORT);
+        Nop();
+    }    
+ CLEARBIT(IFS0bits.T1IF) ;
+    
+    
 }
