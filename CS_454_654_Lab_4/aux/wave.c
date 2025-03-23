eee88a9857efa03c5cca8975f1f0cc218ab67562
TigerLiu <tigerliu@bu.edu>
2025-03-23 18:19:14 -0400

    update: trying timers now, stupid crap

--- CS_454_654_Lab_4/aux/wave.c
+++ CS_454_654_Lab_4/aux/wave.c
@@ -14,6 +14,7 @@ USB DAQ     */
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <signal.h>
 
 u3CalibrationInfo HcaliInfo ;
 
@@ -37,6 +38,15 @@ HANDLE init_DAQ(u3CalibrationInfo * caliInfo)
 struct timespec  res;
 HANDLE newhandle;
 
+
+int state = 0;
+
+void hander_timer1( int signum ){
+    eDO( newhandle, 1 , 2 , state );
+    printf(  "  help!  \n" );
+    }
+    
+
 int main(int argc, char **argv)
 {
 	clock_getres( CLOCK_REALTIME , &res ); 
@@ -46,15 +56,15 @@ int main(int argc, char **argv)
 	newhandle = init_DAQ( &HcaliInfo);
 	printf( " %d \n" , (int) newhandle ) ;
 	int n;
-	int state;
-	state = 0;
+
+
 	/* Provide prompt to the user for a voltage range between 0
 	 * and 5 V. Require a new set of inputs if an invalid range
 	 * has been entered. */
 	 
  	while(1)
 	{
-	    eDO( newhandle, 1 , 2 , state );
+//	    eDO( newhandle, 1 , 2 , state );
 	    eDAC( newhandle, &HcaliInfo , 1, (long) 0, (double) 3 * state , (long) 0, (long) 0, (long) 0);
 	    usleep(50000);
 	    state = 1 - state;
@@ -82,6 +92,30 @@ int main(int argc, char **argv)
 	/* Program a timer to deliver a SIGRTMIN signal, and the
 	 * corresponding signal handler to output a square wave on
 	 * BOTH digital output pin FIO2 and analog pin DAC0. */
+	 
+	timer_t timer1;
+	
+	timer_create( CLOCK_REALTIME, NULL, &timer1 ); 
+
+
+	struct timerspec timer1_time ;
+	
+	timer1_time.it_value.tv_sec = 0;
+	timer1_time.it_value.tv_nsec = 50 * (long) 1000 * 1000;
+	timer1_time.it_value.tv_sec = 0;
+	timer1_time . it_value.tv_nsec = 50 * (long) 1000 * 1000;;
+	
+	struct sigaction actionT1 ;
+	
+	memset( &actionT1 , 0 , sizeof( actionT1 ) ) ;
+	
+	actionT1.sa_handler = hander_timer1 ;
+	
+//	sigaction( SIGALRM, &actionT1 , NULL ) ;
+	
+	timer_settime( timer1, 0, &timer1_time, NULL);
+
+	sigaction( SIGALRM, &actionT1 , NULL ) ;
 
 	/* The square wave generated on the DAC0 analog pin should
 	 * have the voltage range specified by the user in the step
@@ -96,4 +130,6 @@ int main(int argc, char **argv)
 	closeUSBConnection( newhandle );
 	return EXIT_SUCCESS;
 }
+
+
 
