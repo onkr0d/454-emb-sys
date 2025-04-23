bf08111b4f6b2b1a26881c925ab76a2291eb53ee
TigerLiu <tigerliu@bu.edu>
2025-04-23 14:24:11 -0400

    lab 7

--- Lab7.X/pid.py
+++ Lab7.X/pid.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import pybullet as p
 import time
+from scipy import signal
 import numpy as np
 import utils
 import argparse
@@ -19,10 +20,15 @@ class World:
 
 
 state = {
-    "old_x": 0.0,
-    "old_y": 0.0,
-    "old_ex": 0.0,
-    "old_ey": 0.0,
+    "old_x"   :  0.0 ,
+    "old_y"   :  0.0 ,
+    "old_ex"  :  0.0 ,
+    "old_ey"  :  0.0 ,
+    "x_raw"     :  [ ] ,    
+    "y_raw"     :  [ ]    
+
+    
+    
 }
 
 
@@ -44,8 +50,8 @@ def parse_args():
 def run_controller(kp, kd, setpoint, noise, filtered, world: World):
 
     def set_plate_angles(theta_x, theta_y):
-        p.setJointMotorControl2(world.plate, 1, p.POSITION_CONTROL, targetPosition=np.clip(theta_x, -0.1, 0.1), force=5, maxVelocity=2)
-        p.setJointMotorControl2(world.plate, 0, p.POSITION_CONTROL, targetPosition=np.clip(-theta_y, -0.1, 0.1), force=5, maxVelocity=2)
+        p.setJointMotorControl2(world.plate, 1, p.POSITION_CONTROL, targetPosition=np.clip(theta_x, -0.9, 0.9), force=5, maxVelocity=2)
+        p.setJointMotorControl2(world.plate, 0, p.POSITION_CONTROL, targetPosition=np.clip(-theta_y, -0.9, 0.9), force=5, maxVelocity=2)
 
     # you can set the variables that should stay accross control loop here
 
@@ -63,7 +69,7 @@ def run_controller(kp, kd, setpoint, noise, filtered, world: World):
         dex = (ex - state["old_ex"]) / dt # derivative of change in pos, which is just velocity
         dyx = (ey - state["old_ey"]) / dt
 
-        vx, vy, _ = p.getBaseVelocity(world.sphere)[0]
+#        vx, vy, _ = p.getBaseVelocity(world.sphere)[0]
 
         out_x = -kp * ex - kd * dex # equation striaght ripped from slides.
         out_y = -kp * ey - kd * dyx
@@ -76,10 +82,35 @@ def run_controller(kp, kd, setpoint, noise, filtered, world: World):
         return out_x , out_y
 
 
-    def filter_val(val):
+    def filter_val( val ):
         """Implement a filter here, you can use scipy.signal.butter to compute the filter coefficients and then scipy.signal.lfilter to apply the filter.but we recommend you implement it yourself instead of using lfilter because you'll have to do that on the real system later.
         Take a look at the butterworth example written by Renato for inspiration."""
-        pass
+
+
+    # SAmpling rate Hz
+        fc = 100
+
+
+# Butterworth Filter Design ###
+
+# Set the filter order
+        N_ord = 4
+
+# Set the filter cutoff in Hz
+        f_cutoff = 13
+
+# Compute percentage w.r.t. Nyquist frequency
+        wn = f_cutoff * 2/fc
+
+
+# Get the signal coefficients
+        b , a = signal.butter( N_ord , wn , 'low' , analog = False )
+
+# return vector of filtered
+
+
+        return signal.lfilter( b , a , val )
+
 
     def every_10ms(i: int, t: float):
         '''This function is called every ms and performs the following:
@@ -91,10 +122,16 @@ def run_controller(kp, kd, setpoint, noise, filtered, world: World):
         if noise:
             x += utils.noise(t) # the noise added has a frequency between 30 and 50 Hz
             y += utils.noise(t, seed = 43) # so that the noise on y is different than the one on x
+       
         
         if filtered:
-            x = filter_val(x)
-            y = filter_val(y)
+            state["x_raw"] = np.concatenate( ( state["x_raw"] , [ x ]  ) )
+            state["y_raw"] = np.concatenate( ( state["y_raw"] , [ x ]  ) )
+            x_filt = filter_val( state["x_raw"] )
+            y_filt = filter_val( state["y_raw"] )
+            x = x_filt[ -1 ]
+            y = y_filt[ -1 ]
+
 
         (angle_x, angle_y) = pd_controller(x, y, kp, kd, setpoint)
         set_plate_angles(angle_x, angle_y)
@@ -104,8 +141,8 @@ def run_controller(kp, kd, setpoint, noise, filtered, world: World):
 
     utils.loop_every(0.01, every_10ms) # we run our controller at 100 Hz using a linux alarm signal
 
-def run_simulation( initial_ball_position = Point(np.random.uniform(-0.02, -0.019),
-                                                  np.random.uniform(-0.02, -0.019))):
+def run_simulation( initial_ball_position = Point(np.random.uniform(-0.2, 0.2),
+                                                  np.random.uniform(-0.2, 0.2))):
     p.connect(p.GUI)
     p.setAdditionalSearchPath("assets")
     plate = p.loadURDF("plate.urdf")
